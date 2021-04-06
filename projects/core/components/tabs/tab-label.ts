14e1c0d36599e17d903a0885c7c1bf3206762875
sunlightcold <840289075@qq.com>
2021-04-06 15:35:49 +0800

    style(core): 规范 card tabs 样式

--- projects/core/components/tabs/tab-label.ts
+++ projects/core/components/tabs/tab-label.ts
@@ -3,7 +3,6 @@ import { Directive, TemplateRef } from '@angular/core';
 @Directive({
   selector: '[ceuiTabLabel], [ceui-tab-label]',
 })
-// tslint:disable-next-line: directive-class-suffix
 export class CeuiTabLabel {
   constructor(public templateRef: TemplateRef<any>) {}
 }
